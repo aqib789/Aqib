cd7ae37fade7d0b3384dc4b46ca670110f35e592
aqib789 <aqibzaman789@gmail.com>
2017-05-05 00:06:48 +0530

    java2

---
+++ java assignmrnts/ass11/Main1.java
@@ -0,0 +1,24 @@
+package myfirstpackage;
+
+public class Main1 {
+
+public static void main(String[] args) {
+		
+		PermenentEmployee1 pemp=new PermenentEmployee1("John", "HR", 35, 200, 300, 15000);
+		pemp.empdisplay();
+		pemp.setpcode("PM100");
+		pemp.Setphone(98967623);
+		pemp.SetEmpcode(1001);
+		pemp.setgender("Male");
+		pemp.disp();
+		
+		
+		
+		
+		
+
+	}
+
+}
+
+
